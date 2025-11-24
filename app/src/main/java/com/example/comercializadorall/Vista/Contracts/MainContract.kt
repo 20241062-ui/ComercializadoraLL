37673198b970b6fb9a4d90ad3910d8420d6a9915
20241062-ui <20241062@uthh.edu.mx>
2025-11-24 02:08:27 -0600

    prueba exp 12 de muchas

--- app/src/main/java/com/example/comercializadorall/Vista/Contracts/MainContract.kt
+++ app/src/main/java/com/example/comercializadorall/Vista/Contracts/MainContract.kt
@@ -1,9 +1,9 @@
 package com.example.comercializadorall.Vista.Contracts
 
-import com.example.comercializadorall.Modelo.clsProductos
+import com.example.comercializadorall.Presentador.ProductoVista
 
 interface MainContract { // Renombrado
-    fun mostrarProductos(productos: List<clsProductos>) // Renombrado y tipo actualizado
+    fun mostrarProductos(productos: List<ProductoVista>) // Renombrado y tipo actualizado
     fun mostrarError(mensaje: String)
 
 }
